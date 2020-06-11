d7c98f1c4ff49cc0dd8b93170bac6f1063c9a2f6
Eloy <eloydgutierrez@gmail.com>
2020-06-11 17:46:21 -0400

    added code to meet Stretch Goal for node-api4-project so I can deply it on heroku

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express")
 const logger = require("./middleware/logger")
 const userRouter = require("./users/userRouter")
 const postRouter = require("./posts/postRouter")
+// require('dotenv').config() 
+// commented out the above line because I am initiating it in package.json
 
 const server = express()
-const port = 5000
+const port = process.env.PORT
 
 server.use(express.json())
 server.use(logger())
